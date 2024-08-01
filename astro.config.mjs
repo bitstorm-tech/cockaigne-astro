49e72355b28cc065b8a964cb3f0c71f34957f397
Josef Bauer <josef.bauer.1st@gmail.com>
2024-08-01 13:53:05 +0200

    Use vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,11 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
-  server: {
-    host: true,
-  },
+  adapter: vercel(),
 });
 
