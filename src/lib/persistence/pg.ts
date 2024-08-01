1f77fcbf2fb88e439da6bc031dba0b156435cc1d
Josef Bauer <josef.bauer.1st@gmail.com>
2024-08-01 12:06:11 +0200

    Add Postgres SSL handling

--- src/lib/persistence/pg.ts
+++ src/lib/persistence/pg.ts
@@ -5,12 +5,14 @@ const password = import.meta.env.PG_PASSWORD;
 const database = import.meta.env.PG_DATABASE;
 const host = import.meta.env.PG_HOST;
 const port = import.meta.env.PG_PORT;
+const sslMode = import.meta.env.PG_SSL_MODE;
 const sql = postgres({
   user,
   password,
   database,
   host,
   port,
+  ssl: sslMode === "enabled",
 });
 
 console.log(`Connection to database: ${host}:${port}/${database}`);
