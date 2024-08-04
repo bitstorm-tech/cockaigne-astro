4e745269be671e18d5c5f4ce8627e509880ba6b7
Josef Bauer <josef.bauer.1st@gmail.com>
2024-08-05 01:31:03 +0200

    Like and view count only for pro users

--- src/pages/api/deals/viewed/[id].ts
+++ src/pages/api/deals/viewed/[id].ts
@@ -2,7 +2,7 @@ import { incrementDealViewCount } from "@lib/services/deal";
 import type { APIRoute } from "astro";
 
 export const GET: APIRoute = async ({ params, locals }): Promise<Response> => {
-  if (params.id) {
+  if (params?.id && locals.user.id.length > 0) {
     incrementDealViewCount(params.id, locals.user.id);
   }
 
