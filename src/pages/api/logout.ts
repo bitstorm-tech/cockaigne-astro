122fccc2b805c6a34baf3e09f2d949689d40b1ce
Josef Bauer <josef.bauer.1st@gmail.com>
2024-09-05 01:17:05 +0200

    Add missing classes to some icons

--- src/pages/api/logout.ts
+++ src/pages/api/logout.ts
@@ -1,6 +1,7 @@
 export async function GET() {
 	const headers = new Headers();
 	headers.append("Set-Cookie", "jwt=; HttpOnly; Path=/");
-	headers.append("Location", "/");
+	headers.append("Location", "/login");
+
 	return new Response(null, { headers, status: 302 });
 }
